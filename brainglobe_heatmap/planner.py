6bd11bfbebb255ff0fabf3b843b94e47178469a6
Adam Tyson <code@adamltyson.com>
2024-05-21 11:13:59 +0100

    Remove main script

--- brainglobe_heatmap/planner.py
+++ brainglobe_heatmap/planner.py
@@ -116,38 +116,3 @@ class plan(Heatmap):
 
         self.scene.render(interactive=self.interactive, zoom=self.zoom)
         return self.scene
-
-
-if __name__ == "__main__":
-    regions = dict(  # scalar values for each region
-        TH=1,
-        RSP=0.2,
-        AI=0.4,
-        SS=-3,
-        MO=2.6,
-        PVZ=-4,
-        LZ=-3,
-        VIS=2,
-        AUD=0.3,
-        RHP=-0.2,
-        STR=0.5,
-        CB=0.5,
-        FRP=-1.7,
-        HIP=3,
-        PA=-4,
-    )
-
-    plan(
-        regions,
-        # position of the center point of the plane
-        position=(
-            8000,
-            5000,
-            5000,
-        ),
-        # or 'sagittal', or 'horizontal' or a tuple (x,y,z)
-        orientation="frontal",
-        # thickness of the slices used for rendering (in microns)
-        thickness=2000,
-        arrow_scale=750,
-    ).show()
